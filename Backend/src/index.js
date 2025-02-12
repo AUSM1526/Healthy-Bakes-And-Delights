adb1452eca7f8d1251c5afa67e596455666b257f
AUSM1526 <akshatmodi.2212@gmail.com>
2025-02-12 23:02:08 +0530

    `Added cookie-parser and cors dependencies, updated package-lock.json and package.json, and modified app.js and index.js to use these dependencies. Also added utils`

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,25 @@
 import dotenv from "dotenv"
 import connectDB from "./db/db.js";
+import app from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
+.then( () => {
+    console.log("Connected to database");
+
+    app.on("Error: ", (error) => {
+        console.log("Express server error: ",error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at Port: ${process.env.PORT}`);
+    })
+} )
+.catch(error)
+{
+    console.log("DB Connection Error",error);
+}
