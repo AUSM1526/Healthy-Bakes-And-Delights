0dc0e441c968c61c66d3b5cbe292780b9a79600b
AUSM1526 <akshatmodi.2212@gmail.com>
2025-03-11 12:53:47 +0530

    Added new routes

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {registerUser,loginUser,logoutUser,refreshAccessToken} from "../controllers/user.controller.js";
+import {registerUser,loginUser,logoutUser,refreshAccessToken,changePassword,updateName,updateAvatar} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
 
@@ -7,9 +7,12 @@ const userRouter = Router();
 
 userRouter.route("/register").post(upload.single("avatar"),registerUser);
 userRouter.route("/login").post(loginUser);
+userRouter.route("/refresh-token").post(refreshAccessToken);
 
 // secured routes
 userRouter.route("/logout").post(verifyJWT,logoutUser);
-userRouter.route("/refresh-token").post(refreshAccessToken);
+userRouter.route("/change-password").patch(verifyJWT,changePassword);
+userRouter.route("/update-name").patch(verifyJWT,updateName);
+userRouter.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateAvatar);
 
 export default userRouter;
