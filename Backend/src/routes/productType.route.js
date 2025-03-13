318e2fa235fb77e29cd69647c3373239aaad0c80
AUSM1526 <akshatmodi.2212@gmail.com>
2025-03-13 23:53:59 +0530

    minor changes

--- Backend/src/routes/productType.route.js
+++ Backend/src/routes/productType.route.js
@@ -7,7 +7,7 @@ const productTypeRouter = Router();
 
 productTypeRouter.route("/add-product-type").post(verifyJWT,isAdmin,addProductType);
 productTypeRouter.route("/get-all-productTypes").get(verifyJWT,isAdmin,getAllProductTypes);
-productTypeRouter.route("/:productTypeid").patch(verifyJWT,isAdmin,updateProductType);
-productTypeRouter.route("/:productTypeid").delete(verifyJWT,isAdmin,deleteProductType);
+productTypeRouter.route("/:productTypeId").patch(verifyJWT,isAdmin,updateProductType);
+productTypeRouter.route("/:productTypeId").delete(verifyJWT,isAdmin,deleteProductType);
 
 export default productTypeRouter;
