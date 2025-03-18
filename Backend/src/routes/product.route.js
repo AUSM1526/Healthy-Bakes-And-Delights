cd6e99b827549fb1ec06149b3bd5a976bbf78c64
AUSM1526 <akshatmodi.2212@gmail.com>
2025-03-18 18:49:34 +0530

    Added route for fetching products with subcategory

--- Backend/src/routes/product.route.js
+++ Backend/src/routes/product.route.js
@@ -2,10 +2,11 @@ import {Router} from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
 import { isAdmin } from "../middlewares/isAdmin.middleware.js";
 import {upload} from "../middlewares/multer.middleware.js";
-import { createProduct } from "../controllers/product.controller.js";
+import { createProduct, getProductsWithSubcategory } from "../controllers/product.controller.js";
 
 const productRouter = Router();
 
 productRouter.route("/create-product").post(verifyJWT,isAdmin,upload.array("images",5),createProduct);
+productRouter.route("/get-products-with-subcategory").get(getProductsWithSubcategory);
 
 export default productRouter;
