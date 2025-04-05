3ae8fc400e3b62374074ed28c754aff89d254b67
AUSM1526 <akshatmodi.2212@gmail.com>
2025-04-06 01:11:56 +0530

    Refactor JWT verification to return consistent JSON responses for unauthorized access

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -3,13 +3,17 @@ import ApiError from "../utils/ApiError.js";
 import {User} from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
-export const verifyJWT = asyncHandler(async (req, _ , next) => {
+export const verifyJWT = asyncHandler(async (req,res  , next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
         //console.log("verifyJWTToken: ", token);
         //console.log("VerifyJWT: req.body: ", req.body);
         if(!token){
-            throw new ApiError(401, "Unauthorized Request");
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized Request, No access token found"
+            });
+            //throw new ApiError(401, "Unauthorized Request");
         }
 
         const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
@@ -18,12 +22,20 @@ export const verifyJWT = asyncHandler(async (req, _ , next) => {
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
         //console.log("userName: ", user.username);
         if (!user) {
-            throw new ApiError(401, "Invalid Access Token, User not found");
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized Request, User not found"
+            });
+            //throw new ApiError(401, "Invalid Access Token, User not found");
         }
-
+        
         req.user = user;
         next();
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token");
+        //throw new ApiError(401, error?.message || "Invalid access token");
+        return res.status(401).json({
+            success: false,
+            error: error?.message || "Invalid access token"
+        });
     }
 });
