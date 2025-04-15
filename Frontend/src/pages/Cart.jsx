9e2547a0362b8d78cdfb0cafce6d72a4ad36920a
AUSM1526 <akshatmodi.2212@gmail.com>
2025-04-15 17:12:20 +0530

    Add loading spinner to CartPage while fetching cart details

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -6,10 +6,14 @@ import toast from 'react-hot-toast';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../store/authSlice';
 import { useNavigate } from 'react-router-dom';
+import Spinner from '../components/Spinner';
 
 const CartPage = () => {
     const [cart, setCart] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     const fetchCartDetails = async() => {
+        setLoading(true);
         try {
             const res = await apiFunc().get("/user/view-cart");
             const cartdetails = res.data.data.cartItems;
@@ -19,6 +23,8 @@ const CartPage = () => {
         } catch (error) {
             console.log("Error fetching cart details: ", error);
             toast.error(error.response.data.message || "Error fetching cart details");
+        } finally {
+            setLoading(false);
         }
     };
     
@@ -70,73 +76,79 @@ const CartPage = () => {
         <Navbar/>
         <br/>
         <br/>
-        <div className="bg-[#F9F4EF] min-h-screen py-10 px-6 md:px-20 text-[#4b2e1e]">
-        <h1 className="mt-5 text-3xl font-semibold mb-5 text-center font-serif">Your Cart</h1>
-        
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                {/* Cart Items */}
-                <div className="lg:col-span-2 bg-white rounded-lg border shadow-sm">
-                <h2 className="font-serif text-xl font-medium border-b p-4">Cart Items ({totalItems})</h2>
+        {!loading && (
+            <>
+                <div className="bg-[#F9F4EF] min-h-screen py-10 px-6 md:px-20 text-[#4b2e1e]">
+                <h1 className="mt-5 text-3xl font-semibold mb-5 text-center font-serif">Your Cart</h1>
+                
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+                        {/* Cart Items */}
+                        <div className="lg:col-span-2 bg-white rounded-lg border shadow-sm">
+                        <h2 className="font-serif text-xl font-medium border-b p-4">Cart Items ({totalItems})</h2>
 
-                <div className="divide-y">
-                    {cart.map((item, idx) => (
-                    <div key={idx} className="flex items-center justify-between p-4">
-                        <div className="flex items-center gap-4">
-                        <div className="w-16 h-16 bg-[#f3e7da] rounded-md">
-                            <img src={item.images[0]} alt={item.name} className="w-full h-full object-cover rounded-md" />
-                        </div>
-                        <div>
-                            <h3 className="font-serif font-medium">{item.name}<span className="text-[#96705B] text-sm font-light ml-2">{item.productType === "Chocolate" && `${item.subCategory}  ${item.productType}`}</span></h3>
+                        <div className="divide-y">
+                            {cart.map((item, idx) => (
+                            <div key={idx} className="flex items-center justify-between p-4">
+                                <div className="flex items-center gap-4">
+                                <div className="w-16 h-16 bg-[#f3e7da] rounded-md">
+                                    <img src={item.images[0]} alt={item.name} className="w-full h-full object-cover rounded-md" />
+                                </div>
+                                <div>
+                                    <h3 className="font-serif font-medium">{item.name}<span className="text-[#96705B] text-sm font-light ml-2">{item.productType === "Chocolate" && `${item.subCategory}  ${item.productType}`}</span></h3>
+                                </div>
+                                </div>
+
+                                <div className="flex items-center gap-4">
+                                <div className="flex items-center border rounded px-2 py-1">
+                                    <button className="px-1" onClick={() => handleRemoveFromCart(item.productId)}>-</button>
+                                    <span className="px-2">{item.quantity}</span>
+                                    <button className="px-1" onClick={() =>handleAddtoCart(item.productId)}>+</button>
+                                </div>
+                                <div className="w-24 text-right">
+                                    ₹{item.productPrice}.00
+                                </div>
+                                </div>
+                            </div>
+                            ))}
                         </div>
                         </div>
 
-                        <div className="flex items-center gap-4">
-                        <div className="flex items-center border rounded px-2 py-1">
-                            <button className="px-1" onClick={() => handleRemoveFromCart(item.productId)}>-</button>
-                            <span className="px-2">{item.quantity}</span>
-                            <button className="px-1" onClick={() =>handleAddtoCart(item.productId)}>+</button>
+                        {/* Order Summary */}
+                        <div className="bg-white rounded-lg border shadow-sm p-6 h-[290px]">
+                        <h2 className="font-serif text-xl font-medium mb-4">Order Summary</h2>
+                        <div className="space-y-2 text-sm">
+                            <div className="flex justify-between">
+                            {/* <span>Subtotal</span>
+                            <span>₹3700.00</span> */}
+                            </div>
+                            <div className="flex justify-between">
+                            {/* <span>Shipping</span>
+                            <span>₹100.00</span> */}
+                            </div>
+                            <div className="flex justify-between">
+                            {/* <span>Tax</span>
+                            <span>₹185.00</span> */}
+                            </div>
+                            <div className="font-serif text-base flex justify-between font-semibold">
+                            <span>Total</span>
+                            <span>₹{total}.00</span>
+                            </div>
                         </div>
-                        <div className="w-24 text-right">
-                            ₹{item.productPrice}.00
-                        </div>
+
+                        <button className="w-full mt-6 bg-[#4b2e1e] hover:bg-[#5e3b24] text-white py-2 rounded-lg font-medium">
+                            Proceed to Checkout
+                        </button>
+
+                        <button className="w-full mt-3 border border-[#4b2e1e] text-[#4b2e1e] py-2 rounded-lg font-medium hover:bg-[#f4ebe5]" onClick={() => navigate("/products")}>
+                            Continue Shopping
+                        </button>
                         </div>
                     </div>
-                    ))}
                 </div>
-                </div>
+            </>
+        )}
 
-                {/* Order Summary */}
-                <div className="bg-white rounded-lg border shadow-sm p-6 h-[290px]">
-                <h2 className="font-serif text-xl font-medium mb-4">Order Summary</h2>
-                <div className="space-y-2 text-sm">
-                    <div className="flex justify-between">
-                    {/* <span>Subtotal</span>
-                    <span>₹3700.00</span> */}
-                    </div>
-                    <div className="flex justify-between">
-                    {/* <span>Shipping</span>
-                    <span>₹100.00</span> */}
-                    </div>
-                    <div className="flex justify-between">
-                    {/* <span>Tax</span>
-                    <span>₹185.00</span> */}
-                    </div>
-                    <div className="font-serif text-base flex justify-between font-semibold">
-                    <span>Total</span>
-                    <span>₹{total}.00</span>
-                    </div>
-                </div>
-
-                <button className="w-full mt-6 bg-[#4b2e1e] hover:bg-[#5e3b24] text-white py-2 rounded-lg font-medium">
-                    Proceed to Checkout
-                </button>
-
-                <button className="w-full mt-3 border border-[#4b2e1e] text-[#4b2e1e] py-2 rounded-lg font-medium hover:bg-[#f4ebe5]" onClick={() => navigate("/products")}>
-                    Continue Shopping
-                </button>
-                </div>
-            </div>
-        </div>
+        {loading && <Spinner/>}
     </>
   );
 };
