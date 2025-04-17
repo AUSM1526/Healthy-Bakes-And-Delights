56014c908522288647aec7028dd182767f154f1e
AUSM1526 <akshatmodi.2212@gmail.com>
2025-04-17 11:59:05 +0530

    Refactor ProductDisplay component: update layout to use flex-wrap and add responsive card sizing

--- Frontend/src/components/Home/ProductDisplay.jsx
+++ Frontend/src/components/Home/ProductDisplay.jsx
@@ -4,9 +4,14 @@ import HomeProductCard from "./HomeProductCard";
 const ProductDisplay = ({products}) => {
   return (
     <section className="py-12 px-4 sm:px-6 lg:px-10 bg-[#F9F4EF]">
-      <div className="flex space-x-6 overflow-x-auto pb-4">
+      <div className="flex flex-wrap gap-8 justify-center">
         {products.map((product, index) => (
-          <HomeProductCard key={index} {...product} />
+          <div
+            key={index}
+            className="w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33%-1.5rem)] max-w-[400px]"
+          >
+            <HomeProductCard {...product} />
+          </div>
         ))}
       </div>
     </section>
