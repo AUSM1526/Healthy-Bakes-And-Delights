764c4bd10df2ef4df72728816362e917edd0efc2
AUSM1526 <akshatmodi.2212@gmail.com>
2025-05-11 19:32:34 +0530

    Add loading spinner to Home component: implement loading state during initial render

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,22 +3,37 @@ import Navbar from "../components/Navbar";
 import First from "../components/Home/First";
 import Second from "../components/Home/Second";
 import Third from "../components/Home/Third";
+import Spinner from "../components/Spinner";
+import { useState } from "react";
 import { useEffect } from "react";
 
 const Home = () => {
     
     useEffect(() => {
+        simulateLoading();
         window.scrollTo(0, 0);
     }, []);
+
+    const [loading, setLoading] = useState(false);
+
+    const simulateLoading = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+    }
 
     return (
         <>
             <Navbar/>
-            <div className="bg-[#F9F4EF]">
-                <First/>
-                <Second/>
-                <Third/>
-            </div>
+            {loading && <Spinner/>}
+            {!loading && (
+                <div className="bg-[#F9F4EF]">
+                    <First/>
+                    <Second/>
+                    <Third/>
+                </div>
+            )}
         </>
     );
 }
